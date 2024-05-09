d6637286e82ea2f5f19ed7a2ddb54126e52b461c
yaohsiao <yaohsiaopid@gmail.com>
2024-05-08 17:26:48 -0700

    Update exercise1

--- ee282/cva6-renaming/testbenches/exercise1_tb.sv
+++ ee282/cva6-renaming/testbenches/exercise1_tb.sv
@@ -6,16 +6,16 @@
 // instructions. Since these are worked out as part of the written
 // exercise, they are not provided for you here.
 // Input sequence:
-// Cycle  0:  I0:  LI    ar7,   0xa          (VALID)   Commit: None 
-// Cycle  1:  I1:  LI   ar11,   0x4          (VALID)   Commit: I0 
-// Cycle  2:  I2:  LI   ar19,   0xc          (VALID)   Commit: I1 
-// Cycle  3:  I3:  MUL   ar6,   ar7,  ar11   (VALID)   Commit: I2
+// Cycle  0:  I0:  LI   ar11,   0x9          (VALID)   Commit: None
+// Cycle  1:  I1:  ADD   ar3,   ar4,   ar5   (VALID)   Commit: I0
+// Cycle  2:  I2:  LI    ar5,   0x8          (VALID)   Commit: I1
+// Cycle  3:  I3:  MUL   ar5,   ar5,   ar3   (VALID)   Commit: I2
 // Cycle  4:                                           Commit: I3
-// Cycle  5:  I4:  ADD   ar0,   ar0,   ar0   (VALID)   Commit: None
-// Cycle  6:  I5:  MUL   ar7,  ar19,   ar7   (VALID)   Commit: I4 
-// Cycle  7:  I6:  MUL   ar6,  ar11,  ar25   (VALID)   Commit: None
-// Cycle  8:  I7:  ADD   ar7,   ar7,   ar6   (INVALID) Commit: I5
-// Cycle  9:  I8:  SUB   ar7,   ar6,   ar0   (VALID)   Commit: I6
+// Cycle  5:  I4:  SUB   ar8,  ar11,   ar0   (VALID)   Commit: None
+// Cycle  6:  I5:  ADD   ar0,   ar0,   ar0   (VALID)   Commit: I4
+// Cycle  7:  I6:  MUL   ar3,   ar8,  ar25   (INVALID) Commit: I5
+// Cycle  8:  I7:  ADD   ar7,   ar3,   ar6   (VALID)   Commit: None
+// Cycle  9:  I8:  SUB   ar7,   ar7,   ar0   (VALID)   Commit: I7
 // Cycle 10:                                           Commit: I8
 module exercise1_tb import ariane_pkg::*; #(
     parameter int unsigned ARCH_REG_WIDTH = 5,
@@ -45,7 +45,7 @@ module exercise1_tb import ariane_pkg::*; #(
         #10 // Cycle 0
         rst_ni = 1'b1;
         issue_n.valid = 1'b1; // I0
-        issue_n.sbe.rd[PHYS_REG_WIDTH-1:0] = 7; // ar7
+        issue_n.sbe.rd[PHYS_REG_WIDTH-1:0] = 11; // ar11
         issue_n.sbe.rs1[PHYS_REG_WIDTH-1:0] = 1'bx; // don't care
         issue_n.sbe.rs2[PHYS_REG_WIDTH-1:0] = 1'bx; // don't care
         fetch_entry_ready_i = 1'b1;
@@ -57,9 +57,9 @@ module exercise1_tb import ariane_pkg::*; #(
                  issue_q.sbe.rs1[PHYS_REG_WIDTH-1:0],
                  issue_q.sbe.rs2[PHYS_REG_WIDTH-1:0]);
         issue_n.valid = 1'b1; // I1
-        issue_n.sbe.rd[PHYS_REG_WIDTH-1:0] = 11; // ar11 
-        issue_n.sbe.rs1[PHYS_REG_WIDTH-1:0] = 1'bx; // don't care 
-        issue_n.sbe.rs2[PHYS_REG_WIDTH-1:0] = 1'bx; // don't care
+        issue_n.sbe.rd[PHYS_REG_WIDTH-1:0] = 3; // ar3
+        issue_n.sbe.rs1[PHYS_REG_WIDTH-1:0] = 4; // ar4
+        issue_n.sbe.rs2[PHYS_REG_WIDTH-1:0] = 5; // ar5
         fetch_entry_ready_i = 1'b1;
         we_gp_i = 1'b1;
         waddr_i = 0; // I0 commit - replace with I0 physical rd
@@ -69,7 +69,7 @@ module exercise1_tb import ariane_pkg::*; #(
                  issue_q.sbe.rs1[PHYS_REG_WIDTH-1:0],
                  issue_q.sbe.rs2[PHYS_REG_WIDTH-1:0]);
         issue_n.valid = 1'b1; // I2
-        issue_n.sbe.rd[PHYS_REG_WIDTH-1:0] = 19; // ar6
+        issue_n.sbe.rd[PHYS_REG_WIDTH-1:0] = 5; // ar5
         issue_n.sbe.rs1[PHYS_REG_WIDTH-1:0] = 1'bx; // don't care
         issue_n.sbe.rs2[PHYS_REG_WIDTH-1:0] = 1'bx; // don't care
         fetch_entry_ready_i = 1'b1;
@@ -81,9 +81,9 @@ module exercise1_tb import ariane_pkg::*; #(
                  issue_q.sbe.rs1[PHYS_REG_WIDTH-1:0],
                  issue_q.sbe.rs2[PHYS_REG_WIDTH-1:0]);
         issue_n.valid = 1'b1; // I3
-        issue_n.sbe.rd[PHYS_REG_WIDTH-1:0] = 6; // ar6 
-        issue_n.sbe.rs1[PHYS_REG_WIDTH-1:0] = 7; // ar7
-        issue_n.sbe.rs2[PHYS_REG_WIDTH-1:0] = 11; // ar11
+        issue_n.sbe.rd[PHYS_REG_WIDTH-1:0] = 5; // ar5
+        issue_n.sbe.rs1[PHYS_REG_WIDTH-1:0] = 5; // ar5
+        issue_n.sbe.rs2[PHYS_REG_WIDTH-1:0] = 3; // ar3
         fetch_entry_ready_i = 1'b1;
         we_gp_i = 1'b1;
         waddr_i = 0; // I2 commit - replace with I2 physical rd
@@ -98,8 +98,8 @@ module exercise1_tb import ariane_pkg::*; #(
         waddr_i = 0; // I3 commit - replace with I3 physical rd
         #20 // Cycle 5
         issue_n.valid = 1'b1; // I4
-        issue_n.sbe.rd[PHYS_REG_WIDTH-1:0] = 0; // ar0 
-        issue_n.sbe.rs1[PHYS_REG_WIDTH-1:0] = 0; // ar0
+        issue_n.sbe.rd[PHYS_REG_WIDTH-1:0] = 8; // ar8
+        issue_n.sbe.rs1[PHYS_REG_WIDTH-1:0] = 11; // ar11
         issue_n.sbe.rs2[PHYS_REG_WIDTH-1:0] = 0; // ar0
         fetch_entry_ready_i = 1'b1;
         we_gp_i = 1'b0; // No committing instruction
@@ -110,9 +110,9 @@ module exercise1_tb import ariane_pkg::*; #(
                  issue_q.sbe.rs1[PHYS_REG_WIDTH-1:0],
                  issue_q.sbe.rs2[PHYS_REG_WIDTH-1:0]);
         issue_n.valid = 1'b1; // I5
-        issue_n.sbe.rd[PHYS_REG_WIDTH-1:0] = 7; // ar7 
-        issue_n.sbe.rs1[PHYS_REG_WIDTH-1:0] = 19; // ar19 
-        issue_n.sbe.rs2[PHYS_REG_WIDTH-1:0] = 7; // ar7
+        issue_n.sbe.rd[PHYS_REG_WIDTH-1:0] = 0; // ar0
+        issue_n.sbe.rs1[PHYS_REG_WIDTH-1:0] = 0; // ar0
+        issue_n.sbe.rs2[PHYS_REG_WIDTH-1:0] = 0; // ar0
         fetch_entry_ready_i = 1'b1;
         we_gp_i = 1'b1;
         waddr_i = 0; // I4 commit - replace with I4 physical rd
@@ -121,25 +121,25 @@ module exercise1_tb import ariane_pkg::*; #(
                  issue_q.sbe.rd[PHYS_REG_WIDTH-1:0],
                  issue_q.sbe.rs1[PHYS_REG_WIDTH-1:0],
                  issue_q.sbe.rs2[PHYS_REG_WIDTH-1:0]);
-        issue_n.valid = 1'b1; // I6
-        issue_n.sbe.rd[PHYS_REG_WIDTH-1:0] = 6; // ar6
-        issue_n.sbe.rs1[PHYS_REG_WIDTH-1:0] = 11; // ar11
+        issue_n.valid = 1'b0; // I6 invalid
+        issue_n.sbe.rd[PHYS_REG_WIDTH-1:0] = 3; // ar3
+        issue_n.sbe.rs1[PHYS_REG_WIDTH-1:0] = 8; // ar8
         issue_n.sbe.rs2[PHYS_REG_WIDTH-1:0] = 25; // ar25
         fetch_entry_ready_i = 1'b1;
-        we_gp_i = 1'b0; // No committing instruction
-        waddr_i = 0;
+        we_gp_i = 1'b1; 
+        waddr_i = 0;  // I5 commit - replace with I5 physical rd
         #20 // Cycle 8
         $display("I6: rd %d rs1 %d rs2 %d", 
                  issue_q.sbe.rd[PHYS_REG_WIDTH-1:0],
                  issue_q.sbe.rs1[PHYS_REG_WIDTH-1:0],
                  issue_q.sbe.rs2[PHYS_REG_WIDTH-1:0]);
-        issue_n.valid = 1'b0; // I7 invalid
+        issue_n.valid = 1'b1; // I7
         issue_n.sbe.rd[PHYS_REG_WIDTH-1:0] = 7; // ar7
-        issue_n.sbe.rs1[PHYS_REG_WIDTH-1:0] = 7; // ar7
+        issue_n.sbe.rs1[PHYS_REG_WIDTH-1:0] = 3; // ar3
         issue_n.sbe.rs2[PHYS_REG_WIDTH-1:0] = 6; // ar6
         fetch_entry_ready_i = 1'b1;
-        we_gp_i = 1'b1;
-        waddr_i = 0; // I5 commit - replace with I5 physical rd
+        we_gp_i = 1'b0; // No committing instruction
+        waddr_i = 0; 
         #20 // Cycle 9
         $display("I7: rd %d rs1 %d rs2 %d", 
                  issue_q.sbe.rd[PHYS_REG_WIDTH-1:0],
@@ -147,11 +147,11 @@ module exercise1_tb import ariane_pkg::*; #(
                  issue_q.sbe.rs2[PHYS_REG_WIDTH-1:0]);
         issue_n.valid = 1'b1; // I8
         issue_n.sbe.rd[PHYS_REG_WIDTH-1:0] = 7; // ar7
-        issue_n.sbe.rs1[PHYS_REG_WIDTH-1:0] = 6; // ar6
+        issue_n.sbe.rs1[PHYS_REG_WIDTH-1:0] = 7; // ar7
         issue_n.sbe.rs2[PHYS_REG_WIDTH-1:0] = 0; // ar0
         fetch_entry_ready_i = 1'b1;
         we_gp_i = 1'b1;
-        waddr_i = 0; // I6 commit - replace with I6 physical rd
+        waddr_i = 0; // I7 commit - replace with I7 physical rd
         #20 // Cycle 10
         $display("I8: rd %d rs1 %d rs2 %d", 
                  issue_q.sbe.rd[PHYS_REG_WIDTH-1:0],
